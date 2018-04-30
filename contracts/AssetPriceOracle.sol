3e72731310a8560a07a9b1b721340648b633fbf4
Svante Jørgensen <shj@nineconsult.dk>
2018-04-30 17:54:12 +0200

    Contract Solidity update and deploy clean-up.

--- contracts/AssetPriceOracle.sol
+++ contracts/AssetPriceOracle.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.23;
 
 import "./ds-auth/auth.sol"; // https://dappsys.readthedocs.io/en/latest/ds_auth.html
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,11 +3,7 @@ var ContractForDifference = artifacts.require("./ContractForDifference.sol");
 var EIP20Interface = artifacts.require("./EIP20Interface.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(AssetPriceOracle).then(() => {
-    console.log('AssetPriceOracle deployed address: ' + AssetPriceOracle.address);
-  });
   deployer.deploy(ContractForDifference).then(() => {
     console.log('ContractForDifference deployed address: ' + ContractForDifference.address);
   });
-  //deployer.deploy(EIP20Interface);
 };
