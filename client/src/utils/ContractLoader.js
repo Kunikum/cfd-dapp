ec8510d5c1bcb6c7905437f46a6a43776bc4cfd8
Svante Jørgensen <shj@nineconsult.dk>
2018-07-11 16:24:02 +0200

    ContractLoader.js reformat

--- client/src/utils/ContractLoader.js
+++ client/src/utils/ContractLoader.js
@@ -5,16 +5,16 @@ import web3 from './web3';
 
 // Loads the truffle-contract instance of the ContractForDifference 
 export async function getCfdInstance() {
-    const cfd = contract(ContractForDifference);
-    cfd.setProvider(web3.currentProvider);
-    const cfdInstance = await cfd.deployed();
-    return cfdInstance;
+  const cfd = contract(ContractForDifference);
+  cfd.setProvider(web3.currentProvider);
+  const cfdInstance = await cfd.deployed();
+  return cfdInstance;
 }
 
 // Loads the truffle-contract instance of the AssetPriceOracle
 export async function getApoInstance() {
-    const apo = contract(AssetPriceOracle);
-    apo.setProvider(web3.currentProvider);
-    const apoInstance = await apo.deployed();
-    return apoInstance;
+  const apo = contract(AssetPriceOracle);
+  apo.setProvider(web3.currentProvider);
+  const apoInstance = await apo.deployed();
+  return apoInstance;
 }
